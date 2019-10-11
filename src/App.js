5ae16b784ec59ec50e55a31f0c0ee55ab0e085d6
Brian <44780125+BrianEpaud@users.noreply.github.com>
2019-10-11 12:31:38 +0200

    todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      heading:"Shopping List",
+      items: [
+        {text: "Buy Milk", done:false},
+        {text: "Buy Tomatoes", done:false},
+      ]
+    }
+  }
+  markDone = (todo) => {
+    let todos = this.state.items.map(t => {
+      if(t.text == todo.text){
+        return Object.assign({}, t, {
+         done: !t.done
+        } )
+      }
+      return t
+    })
+    this.setState({items: todos})
+  }
+  render() {
+    return (
+      <div className="container">
+        <br />
+        <div className="row">
+          <div className="col-md-6 offset-3">
+            <h1>{this.state.heading}</h1>
+
+            <ul>
+              {this.state.items.map(item => {
+                return(<li  onClick={() => this.markDone(item)} className={item.done ? "done" :"" } key={item.text}>{item.text}</li>)
+              })
+              }
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
